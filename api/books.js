96a46fdf07f1da53f0db71d00e0aa25157f115c5
Erik Langemalm <82160523+ErikLangemalm@users.noreply.github.com>
2024-04-25 22:25:00 +0200

    Downloaded faker to populate database at a later date

--- api/books.js
+++ api/books.js
@@ -1,26 +1,15 @@
 export default function books(server, mongoose) {
-  // Skapar ett schema för "users", vilket definierar strukturen för varje "user"-dokument i databasen.
+
   const booksSchema = new mongoose.Schema({
     title: String,
     author: String,
     rating: Number,
     published: String,
-    information: String
+    information: String,
+    genre: String
   });
 
-  /* 
-    Skapar en Mongoose-modell baserat på usersSchema.
-    Detta tillåter oss att skapa, läsa, uppdatera, och ta bort (CRUD) dokument i vår "users"-collection.
-  */
   const Books = mongoose.model('books', booksSchema);
-
-  /*
-    Skapar en GET - route på '/api/users'. 
-    När denna route anropas, hämtar den alla dokument från vår "users"-collection och skickar tillbaka dem som en JSON-response.
-  */
-  server.get('/api/books', async (req, res) => {
-    res.json(await Books.find());  // Använder Mongoose's "find"-metod för att hämta alla "users".
-  });
 
   server.post('/api/books', async (req, res) => {
     try {
@@ -30,15 +19,30 @@ export default function books(server, mongoose) {
         author,
         rating,
         published,
-        information
+        information,
+        genre
       });
       const savedBook = await newBook.save()
       res.status(201).json(savedBook);
-      console.log("Du har lagt till en ny bok");
+      console.log("You have added a new book");
     }
     catch (err) {
       console.error(err);
       res.status(500).json({ err: 'There was a problem adding a book' })
     }
+  });
+
+  server.get('/api/books', async (req, res) => {
+
+    const { rating } = req.query
+    let filter
+    if (rating) {
+      filter = await Books.find({ rating: rating });
+    }
+    else {
+      filter = await Books.find()
+    }
+    res.json(filter)
+
   });
 }
--- report.md
+++ report.md
@@ -1,2 +1,14 @@
-använde mig av mockoon för att generera data.
-APIt använder sig av MongoDB atlas för att hosta tabellerna
+använde mig av chatgpt för att generera data.
+APIt använder sig av MongoDB atlas för att hosta ett cluster som innehåller tabellen books som i sin tur innehåller titel, författare, betyg, när boken släpptes och en kort sträng
+med information kring boken.
+
+Min api använder sig av mongoose samt express för att generera scheman från
+
+
+### Vad testar vi?
+
+
+ ### Vad visar testerna på - finns det buggar/fel i produkten? 
+
+
+### Saker vi inte hunnit testa
